de73c3f08b9311b18f081f0b747893a2a3d7a7e0
Sem4eZ <semen.fedorov3005@yandex.ru>
2024-02-15 12:24:21 +0500

    Неизвестный пользователь - перенаправление на главную

--- src/components/Employees/ProfileEmployee.jsx
+++ src/components/Employees/ProfileEmployee.jsx
@@ -1,16 +1,11 @@
 import React, { useState, useEffect, useNavigate } from "react";
-// import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import LinksPages from "../LinksPages";
 
 const ProfileEmployee = () => {
-  //   const { employees } = useSelector((state) => state.employee);
   const { id } = useParams();
-  //   const navigate = useNavigate();
   const [employee, setEmployee] = useState([]);
-  console.log(id);
-
   useEffect(() => {
     async function fetchEmployees() {
       try {
@@ -22,7 +17,7 @@ const ProfileEmployee = () => {
         alert(
           "К сожалению мы не смогли найти вашего работника, попробуйте выбрать другого"
         );
-        // navigate("/");
+        window.location.href = "/";
       }
     }
 
@@ -30,16 +25,46 @@ const ProfileEmployee = () => {
   }, []);
 
   return (
-    <div className="container">
+    <div className="mx-auto max-w-[1590px] px-6">
       <LinksPages name={employee.name} />
-      <div>{employee.name}</div>
-      <img src={employee.photo} alt="" />
-      <div>{employee.phone}</div>
-      <div>{employee.gender}</div>
-      <div>{employee.position}</div>
-      <div>{employee.stack}</div>
-      <div>{employee.birthdate}</div>
-      <div>{employee.dateOfEmployment}</div>
+      <div className="flex">
+        <img
+          src={employee.photo}
+          alt=""
+          className="rounded-full w-[105px] h-[105px]"
+        />
+        <div className="ml-4">
+          <div className="font-bold text-xl mb-2">{employee.name}</div>
+          <div className="font-medium text-sm">{employee.position}</div>
+        </div>
+      </div>
+      <div>
+        {employee.stack && employee.stack.length > 0 && (
+          <div className="flex gap-2">
+            {employee.stack.map((stackItem, index) => (
+              <div
+                key={index}
+                className="bg-[#F2F2F2] py-2.5 px-4 rounded-md mb-5 mt-3"
+              >
+                {stackItem}
+              </div>
+            ))}
+          </div>
+        )}
+
+        <hr />
+      </div>
+
+      <div >
+        <div>Основная информация</div>
+        <div>
+          <div>{employee.phone}</div>
+          <div>{employee.gender}</div>
+
+          <div>{employee.birthdate}</div>
+          <div>{employee.dateOfEmployment}</div>
+        </div>
+      </div>
     </div>
   );
 };
